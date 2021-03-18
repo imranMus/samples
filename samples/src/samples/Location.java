17bca047f06fdfc5f6f532cadf093b294d1dd5eb
Imran <muhdimran1810@gmail.com>
2021-03-18 13:36:50 +0800

    New attributes

--- samples/src/samples/Location.java
+++ samples/src/samples/Location.java
@@ -1,13 +1,24 @@
 package samples;
 
 public class Location {
-	private String loc;
-
-	public String getLoc() {
-		return loc;
+	private String Name;
+	private int locationId, description;
+	public String getName() {
+		return Name;
 	}
-
-	public void setLoc(String loc) {
-		this.loc = loc;
+	public void setName(String name) {
+		Name = name;
+	}
+	public int getLocationId() {
+		return locationId;
+	}
+	public void setLocationId(int locationId) {
+		this.locationId = locationId;
+	}
+	public int getDescription() {
+		return description;
+	}
+	public void setDescription(int description) {
+		this.description = description;
 	}
 }
